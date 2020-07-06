a5b3c06494e036cf38dd84941b04a92dba4e0746
Ameer Hamza Tariq <ahamzatariq@gmail.com>
2020-07-06 23:26:04 +0500

    ~ Shop App ~
    - Added another Focus node to next TextField
    
    New Concepts:
    + TextInputType.multiline
    + TextFormField(maxLines: 3)

--- lib/pages/edit_product_page.dart
+++ lib/pages/edit_product_page.dart
@@ -11,6 +11,7 @@ class EditProductPage extends StatefulWidget {
 
 class _EditProductPageState extends State<EditProductPage> {
   final _priceFocusNode = FocusNode();
+  final _descriptionFocusNode = FocusNode();
 
   @override
   Widget build(BuildContext context) {
@@ -24,6 +25,7 @@ class _EditProductPageState extends State<EditProductPage> {
           child: ListView(
             children: <Widget>[
               TextFormField(
+                autofocus: true,
                 decoration: InputDecoration(labelText: 'Title'),
                 textInputAction: TextInputAction.next,
                 onFieldSubmitted: (_) =>
@@ -34,11 +36,27 @@ class _EditProductPageState extends State<EditProductPage> {
                 textInputAction: TextInputAction.next,
                 keyboardType: TextInputType.number,
                 focusNode: _priceFocusNode,
+                onFieldSubmitted: (_) =>
+                    FocusScope.of(context).requestFocus(_descriptionFocusNode),
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Description'),
+                maxLines: 3,
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.multiline,
+                focusNode: _descriptionFocusNode,
               ),
             ],
           ),
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _priceFocusNode.dispose();
+    _descriptionFocusNode.dispose();
+    super.dispose();
   }
 }
