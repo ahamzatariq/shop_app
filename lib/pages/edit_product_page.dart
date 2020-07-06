d8e8224e291e85a842aebcba04688d97c81d7474
Ameer Hamza Tariq <ahamzatariq@gmail.com>
2020-07-07 01:58:12 +0500

    ~ Shop App ~
    - Saving new Products via EditProductsPage
    
    New Concepts:
    +

--- lib/pages/edit_product_page.dart
+++ lib/pages/edit_product_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:shop_app/providers/product.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/product.dart';
+import '../providers/products.dart';
 
 class EditProductPage extends StatefulWidget {
   static const routeName = '/edit-product';
@@ -48,10 +51,8 @@ class _EditProductPageState extends State<EditProductPage> {
       return;
     }
     _form.currentState.save();
-    print(_editedProduct.title);
-    print(_editedProduct.description);
-    print(_editedProduct.price);
-    print(_editedProduct.imageUrl);
+    Provider.of<Products>(context, listen: false).addProduct(_editedProduct);
+    Navigator.of(context).pop();
   }
 
   @override
--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -64,7 +64,15 @@ class Products with ChangeNotifier {
     return _items.firstWhere((product) => product.id == id);
   }
 
-  void addProduct() {
+  void addProduct(Product product) {
+    final newProduct = Product(
+      title: product.title,
+      description: product.description,
+      imageUrl: product.imageUrl,
+      price: product.price,
+      id: DateTime.now().toString(),
+    );
+    _items.add(product);
 //    _items.add(value);
     notifyListeners();
   }
