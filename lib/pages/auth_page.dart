32c5f0d73ee3bac9b915c6e3279b5ce361c6a957
Ameer Hamza Tariq <ahamzatariq@gmail.com>
2020-07-10 18:53:44 +0500

    ~ Shop App ~
    - Instead of listening for your object manually and using setState, use AnimatedBuilder Widget.
    
    New Concepts:
    + AnimatedBuilder()

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -115,14 +115,14 @@ class _AuthCardState extends State<AuthCard>
       duration: Duration(milliseconds: 500),
     );
     _heightAnimation = Tween<Size>(
-            begin: Size(double.infinity, 260), end: Size(double.infinity, 320))
+            begin: Size(double.infinity, 260), end: Size(double.infinity, 320),)
         .animate(
       CurvedAnimation(
         parent: _controller,
         curve: Curves.fastLinearToSlowEaseIn,
       ),
     );
-    _heightAnimation.addListener(() => setState(() {}));
+//    _heightAnimation.addListener(() => setState(() {}));
   }
 
   @override
@@ -187,7 +187,7 @@ class _AuthCardState extends State<AuthCard>
       }
       _showErrorDialog(errorMessage);
     } catch (error) {
-      var errorMessage = 'Could not authenticate. Please try again later.';
+//      var errorMessage = 'Could not authenticate. Please try again later.';
     }
 
     setState(() {
@@ -217,13 +217,17 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: Container(
+      child: AnimatedBuilder(
+        animation: _heightAnimation,
+        builder: (context, myChild) => Container(
 //        height: _authMode == AuthMode.Signup ? 320 : 260,
-        height: _heightAnimation.value.height,
-        constraints:
-            BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
-        width: deviceSize.width * 0.75,
-        padding: EdgeInsets.all(16.0),
+          height: _heightAnimation.value.height,
+          constraints: BoxConstraints(
+              minHeight: _authMode == AuthMode.Signup ? 320 : 260),
+          width: deviceSize.width * 0.75,
+          padding: EdgeInsets.all(16.0),
+          child: myChild,
+        ),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
